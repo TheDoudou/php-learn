20c7bb7c180f5d3f3f91682f64bb4d81d89e549b
TheDoudou <thechikan@mac.com>
2018-11-29 16:55:31 +0100

    Fix collapse bug

--- lib/controleur/header.php
+++ lib/controleur/header.php
@@ -15,8 +15,8 @@
 
 <div class="header-blue">
     <nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
-        <div class="container"><a class="navbar-brand nav-white" href="#">Les animaux</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1">
+        <div class="container"><a class="navbar-brand nav-white" href="#">Les animaux</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-2"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-2">
                 <ul class="nav navbar-nav mr-auto">
                     <li class="nav-item" role="presentation"><a class="nav-link nav-white" href="index.php?view=12&page=lion.php">Lion</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link nav-white" href="index.php?view=12&page=impala.php">Impala</a></li>
